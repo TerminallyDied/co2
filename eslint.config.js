7c19721a611c9f9885fb2831e1c75d0a18ceeb26
Gamzee <lithiumturkin@protonmail.com>
2025-12-01 09:14:41 +0100

    Feat: Add eslint stylistic

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,14 @@
 // https://docs.expo.dev/guides/using-eslint/
 const { defineConfig } = require('eslint/config');
 const expoConfig = require('eslint-config-expo/flat');
+const stylistic = require('@stylistic/eslint-plugin')
 
 module.exports = defineConfig([
   expoConfig,
   {
+    plugins: {
+      '@stylistic': stylistic
+    },
     ignores: ['dist/*'],
   },
 ]);
